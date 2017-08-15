0feb781182c02d325a98bd436e2116e1e948c5f1
Javier Cabezas <jcleyton@gmail.com>
2017-08-15 17:06:45 -0300

    Fixed vue warning on v-for

--- src/components/main/footer/FooterBlockList.vue
+++ src/components/main/footer/FooterBlockList.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="f-block-list">
-        <footer-block v-for="block in blocks" :block="block"></footer-block>
+        <footer-block v-for="b in blocks" :key="b.path" :block="b"></footer-block>
     </div>
 </template>
 
@@ -10,36 +10,36 @@
     export default {
         data () {
             return {
-                blocks: [
-                    {
+                blocks: {
+                    1: {
                         "img_src": "http://placehold.it/300x127",
                         "icon_name": "home",
                         "section_name": "Inicio",
                         "section_subtext": "Últimos productos, promociones y novedades",
                         "path": "/"
                     },
-                    {
+                    2: {
                         "img_src": "http://placehold.it/300x127",
                         "icon_name": "question",
                         "section_name": "Sobre Nosotros",
                         "section_subtext": "Información comercial y de nuestra empresa",
                         "path": "sobre-nosotros"
                     },
-                    {
+                    3: {
                         "img_src": "http://placehold.it/300x127",
                         "icon_name": "shopping-bag",
                         "section_name": "Productos",
                         "section_subtext": "Listdo de productos disponibles para su venta",
                         "path": "productos"
                     },
-                    {
+                    4: {
                         "img_src": "http://placehold.it/300x127",
                         "icon_name": "envelope",
                         "section_name": "Contacto",
                         "section_subtext": "Ponte en contacto con nosotros.",
                         "path": "contacto"
                     }
-                ],
+                },
             }
         },
         components: {
