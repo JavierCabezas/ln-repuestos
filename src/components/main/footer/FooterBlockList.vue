07afd5c80bc8e989d7ddea9dc6ff67e1b488488a
Fernanda Kauak <fernanda.kauak@gmail.com>
2017-08-15 23:38:00 -0300

    Cambios realizados 15-08
    Imágenes de footer, mapas y bloque de suscripción

--- src/components/main/footer/FooterBlockList.vue
+++ src/components/main/footer/FooterBlockList.vue
@@ -12,28 +12,28 @@
             return {
                 blocks: {
                     1: {
-                        "img_src": "http://placehold.it/300x127",
+                        "img_src": "src/assets/img/footer/footer-001.png",
                         "icon_name": "home",
                         "section_name": "Inicio",
                         "section_subtext": "Últimos productos, promociones y novedades",
                         "path": "/"
                     },
                     2: {
-                        "img_src": "http://placehold.it/300x127",
+                        "img_src": "src/assets/img/footer/footer-002.png",
                         "icon_name": "question",
                         "section_name": "Sobre Nosotros",
                         "section_subtext": "Información comercial y de nuestra empresa",
                         "path": "sobre-nosotros"
                     },
                     3: {
-                        "img_src": "http://placehold.it/300x127",
+                        "img_src": "src/assets/img/footer/footer-003.png",
                         "icon_name": "shopping-bag",
                         "section_name": "Productos",
                         "section_subtext": "Listdo de productos disponibles para su venta",
                         "path": "productos"
                     },
                     4: {
-                        "img_src": "http://placehold.it/300x127",
+                        "img_src": "src/assets/img/footer/footer-004.png",
                         "icon_name": "envelope",
                         "section_name": "Contacto",
                         "section_subtext": "Ponte en contacto con nosotros.",
