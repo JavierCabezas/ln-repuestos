f03d3eeb66a6e187c23214a92289bdbff46720e8
Javier Cabezas <jcleyton@gmail.com>
2017-08-19 16:21:31 -0300

    Added sweetalert message on successfull subscription

--- src/components/main/Newsletter.vue
+++ src/components/main/Newsletter.vue
@@ -1,15 +1,17 @@
 <template>
     <div class="newsletter">
         <h3>Subscríbete a nosotros</h3>
-        <p>Ingresa tu dirección de correo electrónico para recibir ofertas</p>
+        <p>Ingresa tu dirección de correo electrónico para recibir ofertas y promociones exclusivas.</p>
         <form action="#">
-            <input placeholder="Tu dirección de correo electrónico" type="email" v-model="input_mail">
-            <input name="OK" value="Subscribirse" type="submit" @click.prevent="add_to_database()">
+            <input class="form-control" placeholder="Tu dirección de correo electrónico" type="email" v-model="input_mail">
+            <input class="form-control" name="OK" value="Subscribirse" type="submit" @click.prevent="add_to_database()">
         </form>
     </div>
 </template>
 
 <script>
+    require('sweetalert');
+
     export default {
         data () {
             return {
@@ -17,13 +19,26 @@
             }
         },
         methods: {
+            is_email: function(email) {
+                let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+                return reg.test(email)
+            },
             add_to_database: function() {
-                $.ajax({
-                    url: vm.url_backend + 'newsletter/add',
-                    success: function (result) {
-                        vm.items = result;
-                    }
-                });
+                let vm = this;
+
+                if(!vm.is_email(vm.input_mail)){
+                    swal("Oops...", "La dirección de correo electrónico ingresada no es correcta.", "error");
+                }else{
+                    $.ajax({
+                        url: vm.url_backend + 'newsletter/add',
+                        data: { client_email: vm.input_mail },
+                        method: 'POST',
+                        success: function (result) {
+                            vm.input_mail = '';
+                            swal("Éxito", "Agregamos su dirección correctamente, ¡Gracias!", "success")
+                        }
+                    });
+                }
             }
         }
     }
