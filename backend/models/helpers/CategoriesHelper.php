5a8b7d25485c233e9b26a22c2ebc900049b8bc5c
Javier Cabezas <jcleyton@gmail.com>
2017-08-29 21:42:40 -0300

    Real data for categories and subcategories

--- backend/models/helpers/CategoriesHelper.php
+++ backend/models/helpers/CategoriesHelper.php
@@ -9,62 +9,230 @@
 class CategoriesHelper{
     private static $categories = [
         [
-            'n' => 'Padre 1',
-            'l' => 'padre_1',
+            'n' => 'Motor',
+            'l' => 'productos/motor',
             's' => [
                 [
-                    'n' => 'Motor',
-                    'l' => 'motor'
+                    'n' => 'Block',
+                    'l' => 'productos/motor/block',
+                    's' => [
+                        [
+                            'n' => 'Empaquetaduras',
+                            'l' => 'produtos/motor/block/empaquetaduras'
+                        ],
+                        [
+                            'n' => 'Retenes / Anillos',
+                            'l' => 'produtos/motor/retenes-anillos'
+                        ],
+                        [
+                            'n' => 'Pistones',
+                            'l' => 'produtos/motor/block/pistones'
+                        ],
+                        [
+                            'n' => 'Carter',
+                            'l' => 'produtos/motor/block/carter'
+                        ],
+                        [
+                            'n' => 'Metales',
+                            'l' => 'produtos/motor/block/metales'
+                        ]
+                    ]
                 ],
                 [
-                    'n' => 'Amortiguación',
-                    'l' => 'amortiguacion'
+                    'n' => 'Correas',
+                    'l' => 'produtos/motor/correas',
+                    's' => [
+                        [
+                            'n' => 'Distribución',
+                            'l' => 'produtos/motor/correas/distribucion'
+                        ],
+                        [
+                            'n' => 'Accesorios',
+                            'l' => 'produtos/motor/correas/accesorios'
+                        ],
+                    ]
                 ],
                 [
-                    'n' => 'Filtros',
-                    'l' => 'filtros'
-                ],
-            ]
-        ],
-        [
-            'n' => 'Padre 2',
-            'l' => 'padre_2',
-            's' => [
-                [
-                    'n' => 'Soportes de motor',
-                    'l' => 'soportes_motor'
+                    'n' => 'Soportes motor',
+                    'l' => 'productos/motor/soportes-motor',
                 ],
                 [
-                    'n' => 'Eléctricos',
-                    'l' => 'electricos'
-                ]
-            ]
-        ],
-        [
-            'n' => 'Padre 3',
-            'l' => 'padre_3',
-            's' => [
-                [
-                    'n' => 'Accesorios',
-                    'l' => 'Caja de cambios'
+                    'n' => 'Sistema de lubricación',
+                    'l' => 'productos/motor/sistema-de-lubricacion',
+                    's' => [
+                        [
+                            'n' => 'Filtros',
+                            'l' => 'produtos/motor/sistema-de-lubricacion/filtros'
+                        ],
+                        [
+                            'n' => 'Bombas',
+                            'l' => 'produtos/motor/sistema-de-lubricacion/bombas'
+                        ],
+                    ]
                 ],
                 [
-                    'n' => 'Frenos',
-                    'l' => 'frenos'
-                ]
-            ]
-        ],
-        [
-            'n' => 'Padre 4',
-            'l' => 'padre_4',
-            's' => [
-                [
-                    'n' => 'Refrigeración',
-                    'l' => 'refrigeracion'
+                    'n' => 'Enfriamiento',
+                    'l' => 'productos/motor/enfriamiento',
+                    's' => [
+                        [
+                            'n' => 'Radiadores',
+                            'l' => 'productos/motor/enfriamiento/radiadores'
+                        ],
+                        [
+                            'n' => 'Depósitos de refrigerante',
+                            'l' => 'productos/motor/enfriamiento/depositos-refrigerante'
+                        ],
+                        [
+                            'n' => 'Bombas',
+                            'l' => 'productos/motor/enfriamiento/bombas'
+                        ]
+                    ]
                 ],
                 [
                     'n' => 'Encendido',
-                    'l' => 'encendido'
+                    'l' => 'productos/motor/encendido',
+                    's' => [
+                        [
+                            'n' => 'Bobinas',
+                            'l' => 'productos/motor/encendido/bobinas'
+                        ],
+                        [
+                            'n' => 'Bujías',
+                            'l' => 'productos/motor/encendido/bujias'
+                        ],
+                        [
+                            'n' => 'Cables',
+                            'l' => 'productos/motor/encendido/cables'
+                        ]
+                    ]
+                ],
+                [
+                    'n' => 'Combustion',
+                    'l' => 'productos/motor/combustion',
+                    's' => [
+                        [
+                            'n' => 'Filtro aire',
+                            'l' => 'productos/motor/combustion/filtro-aire'
+                        ],
+                        [
+                            'n' => 'Filtro combustible',
+                            'l' => 'productos/motor/combustion/filtro-combustible'
+                        ]
+                    ]
+                ],
+                [
+                    'n' => 'Otros',
+                    'l' => 'productos/motor/otros'
+                ]
+            ]
+        ],
+        [
+            'n' => 'Transmisión',
+            'l' => 'productos/transision',
+            's' => [
+                [
+                    'n' => 'Lubicantes',
+                    'l' => 'productos/transmision/lubricantes',
+                    's' => [
+                        [
+                            'n' => 'Aceite',
+                            'l' => 'productos/transmision/lubricantes/aceite'
+                        ]
+                    ]
+                ],
+                [
+                    'n' => 'Otros',
+                    'l' => 'productos/transmision/otros'
+                ]
+            ]
+        ],
+        [
+            'n' => 'Sistema eléctrico',
+            'l' => 'productos/sistema-electrico',
+            's' => [
+                [
+                    'n' => 'Luces',
+                    'l' => 'productos/sistema-electrico/luces',
+                    's' => [
+                        [
+                            'n' => 'Intermitente',
+                            'l' => 'productos/sistema-electrico/luces/intermitente'
+                        ]
+                    ]
+                ],
+                [
+                    'n' => 'Otros',
+                    'l' => 'productos/sistema-electrico/otros'
+                ]
+            ]
+        ],
+        [
+            'n' => 'Frenos',
+            'l' => 'productos/frenos',
+            's' => [
+                [
+                    'n' => 'Pastillas',
+                    'l' => 'productos/frenos/pastillas',
+                    's' => [
+                        [
+                            'n' => 'Delanteras',
+                            'l' => 'productos/frenos/pastillas/delanteras'
+                        ]
+                    ]
+                ],
+                [
+                    'n' => 'Discos',
+                    'l' => 'productos/frenos/discos'
+                ],
+                [
+                    'n' => 'Otros',
+                    'l' => 'productos/frenos/otros'
+                ]
+            ]
+        ],
+        [
+            'n' => 'Susprensión',
+            'l' => 'productos/suspension',
+            's' => [
+                [
+                    'n' => 'Bandejas',
+                    'l' => 'productos/suspension/bandejas'
+                ],
+                [
+                    'n' => 'Bujes',
+                    'l' => 'productos/suspension/bujes'
+                ],
+                [
+                    'n' => 'Terminales',
+                    'l' => 'productos/suspension/terminales'
+                ],
+                [
+                    'n' => 'Amortiguadores',
+                    'l' => 'productos/suspension/amortiguadores'
+                ],
+                [
+                    'n' => 'Otros',
+                    'l' => 'productos/suspension/otros'
+                ]
+            ]
+        ],
+        [
+            'n' => 'Accesorios',
+            'l' => 'productos/accesorios',
+            's' => [
+                [
+                    'n' => 'Interior',
+                    'l' => 'productos/accesorios/interior'
+                ],
+                [
+                    'n' => 'Exterior',
+                    'l' => 'productos/accesorios/exterior',
+                    's' => [
+                        [
+                            'n' => 'Espejo retrovisor',
+                            'l' => 'productos/accesorios/exterior/espejo-retrovisor'
+                        ]
+                    ]
                 ]
             ]
         ]
--- src/components/main/top/TopMenu.vue
+++ src/components/main/top/TopMenu.vue
@@ -11,6 +11,11 @@
                             <ul>
                                 <li v-for="son in cat.s">
                                     <router-link :to="son.l"> {{son.n}} </router-link>
+                                    <ul v-if="son.s !== undefined">
+                                        <li v-for="grandon in son.s">
+                                            <router-link :to="grandon.l"> {{grandon.n}} </router-link>
+                                        </li>
+                                    </ul>
                                 </li>
                             </ul>
                         </li>
