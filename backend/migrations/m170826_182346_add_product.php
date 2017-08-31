200bcd41a87186c41d5255738b951a0a8fb48e28
Javier Cabezas <jcleyton@gmail.com>
2017-08-30 21:12:34 -0300

    Migrations changes on the products table

--- backend/migrations/m170826_182346_add_product.php
+++ backend/migrations/m170826_182346_add_product.php
@@ -8,12 +8,13 @@ class m170826_182346_add_product extends Migration
     {
         $this->createTable('product', [
             'id' => $this->primaryKey(),
-            'category_id' => $this->integer(),
+            'category_id' => $this->string(30),
             'name' => $this->string(60)->notNull(),
             'seo_name' => $this->string(60),
             'description' => $this->string(300)->notNull(),
             'price' => $this->integer(),
             'is_featured' => $this->boolean()->defaultValue(false),
+            'is_ready' => $this->boolean()->defaultValue(false),
             'created_on' => $this->dateTime()->defaultValue(date('Y-m-d h:i:s'))
         ]);
     }
