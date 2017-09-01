d04fb4dff39fecd4b03b0ebadf31918b3b6f65fb
Javier Cabezas <jcleyton@gmail.com>
2017-08-31 21:50:16 -0300

    Made the main links at the bottom a little bigger

--- src/components/main/footer/FooterMenu.vue
+++ src/components/main/footer/FooterMenu.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div v-for="c in categories" class="f-menu">
-            <router-link :to="{name:'products_category', params: {category: c.id}}"> {{c.n}} </router-link>
+            <h4><router-link :to="{name:'products_category', params: {category: c.id}}"> {{c.n}} </router-link></h4>
             <ul class="nav nav-pills nav-stacked" v-for="son in c.s">
                 <li>
                     <router-link :to="{name:'products_subcategory', params: {category: c.id, subcategory: son.id }}"> {{son.n }}</router-link>
