6d51701795053f639c82b3b14bac6473a910b155
Javier Cabezas <jcleyton@gmail.com>
2017-09-02 17:22:43 -0300

    Deleted unused method in slider controller

--- backend/controllers/SliderController.php
+++ backend/controllers/SliderController.php
@@ -82,25 +82,6 @@ class SliderController extends Controller
     }
 
     /**
-     * Updates an existing Slider model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
      * Deletes an existing Slider model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
      * @param integer $id
