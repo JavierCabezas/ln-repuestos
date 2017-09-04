c8ca8316a28273308bf966f28f3660195ccf5779
Javier Cabezas <jcleyton@gmail.com>
2017-09-03 23:18:18 -0300

    Added whatsapp icon on top menu

--- src/components/main/top/TopBar.vue
+++ src/components/main/top/TopBar.vue
@@ -4,7 +4,7 @@
             <ul class="contactinfo nav nav-pills">
                 <li>
                     <a href="tel:56956433982" target="_top">
-                        <i class='fa fa-phone'></i> +56 9 56433982
+                        <i class='fa fa-phone'></i>/<i class='fa fa-whatsapp'></i>  +56 9 56433982
                     </a>
                 </li>
                 <li>
