bfaf76f917b35a1c2472fc93a1b3e13135fa8471
Javier Cabezas <jcleyton@gmail.com>
2019-03-28 21:09:36 -0300

    Removed subsubcategory from menu

--- src/components/main/top/TopMenu.vue
+++ src/components/main/top/TopMenu.vue
@@ -11,12 +11,6 @@
                             <ul>
                                 <li v-for="son in cat.s">
                                     <router-link :to="{name:'products_subcategory', params: { subcategory: son.id}}"> {{son.n}} </router-link>
-                                    <i v-if="son.s !== undefined" class="fa fa-angle-right"></i>
-                                    <ul v-if="son.s !== undefined">
-                                        <li v-for="grandson in son.s">
-                                            <router-link :to="{name: 'products_subsubcategory', params: { subsubcategory: grandson.id }}"> {{grandson.n}} </router-link>
-                                        </li>
-                                    </ul>
                                 </li>
                             </ul>
                         </li>
