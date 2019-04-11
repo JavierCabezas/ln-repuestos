041fc99c611fd93711afe3852f6955126af1b35b
Javier Cabezas <jcleyton@gmail.com>
2019-04-11 13:11:07 -0400

    Changed bottom pictures names

--- src/components/main/footer/FooterBlockList.vue
+++ src/components/main/footer/FooterBlockList.vue
@@ -12,28 +12,28 @@
             return {
                 blocks: {
                     1: {
-                        "img_src": "src/assets/img/footer/footer-001.png",
+                        "img_src": "src/assets/img/footer/inicio-manubrio.png",
                         "icon_name": "home",
                         "section_name": "Inicio",
                         "section_subtext": "Últimos productos, promociones y novedades",
                         "path": "/"
                     },
                     2: {
-                        "img_src": "src/assets/img/footer/footer-002.png",
+                        "img_src": "src/assets/img/footer/sobre-nosotros.png",
                         "icon_name": "question",
                         "section_name": "Sobre Nosotros",
                         "section_subtext": "Información comercial y de nuestra empresa",
                         "path": "sobre-nosotros"
                     },
                     3: {
-                        "img_src": "src/assets/img/footer/footer-003.png",
+                        "img_src": "src/assets/img/footer/listado-productos.png",
                         "icon_name": "shopping-bag",
                         "section_name": "Productos",
                         "section_subtext": "Listdo de productos disponibles para su venta",
                         "path": "productos"
                     },
                     4: {
-                        "img_src": "src/assets/img/footer/footer-004.png",
+                        "img_src": "src/assets/img/footer/contacto.png",
                         "icon_name": "envelope",
                         "section_name": "Contacto",
                         "section_subtext": "Ponte en contacto con nosotros.",
