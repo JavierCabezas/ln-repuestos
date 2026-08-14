70341cde009a84c7d74f19e6343e773eb59bc7fc
Javier Cabezas <javier.cabezas@betr.app>
2026-08-13 22:48:07 -0400

    fix docker

--- backend/config/db.php
+++ backend/config/db.php
@@ -1,9 +1,8 @@
 <?php
 $secrets = require(__DIR__ . '/secrets.php');
-
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname='.$secrets['database_name'],
+    'dsn' => 'mysql:host=db;port=3306;dbname='.$secrets['database_name'],
     'username' => $secrets['database_user'],
     'password' => $secrets['database_password'],
     'charset' => 'utf8',
--- backend/config/secrets.php
+++ backend/config/secrets.php
@@ -1,14 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: javier
- * Date: 8/18/17
- * Time: 12:12 AM
- */
-
 return [
-    'cookie_validation' => '',
-    'database_user'     => '',
-    'database_password' => '',
-    'database_name'     => ''
+    'cookie_validation' => 'dev-cookie-validation-key-please-change',
+    'database_user'     => 'lnrepuestos',
+    'database_password' => 'lnrepuestos',
+    'database_name'     => 'lnrepuestos',
 ];
